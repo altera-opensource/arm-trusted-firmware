6ae027efea305ab6e35c35d1ac121036390e3bc9
Hadi Asyrafi <muhammad.hadi.asyrafi.abdul.halim@intel.com>
2019-10-22 12:59:32 +0800

    intel: stratix10: Enable uboot entrypoint support
    This patch will provide an entrypoint for uboot's spl into BL31.
    BL31 will also handle secondary cpu state during uboot's cold boot
    
    Signed-off-by: Hadi Asyrafi <muhammad.hadi.asyrafi.abdul.halim@intel.com>
    Change-Id: I661bdb782c2d793d5fc3c7f78dd7ff746e33b7a3

--- plat/intel/soc/common/include/platform_def.h
+++ plat/intel/soc/common/include/platform_def.h
@@ -28,6 +28,9 @@
 #define PLAT_CPUID_RELEASE			0xffe1b000
 #define PLAT_SEC_ENTRY				0xffe1b008
 
+/* sysmgr.boot_scratch_cold4 & 5 used for CPU release address for SPL */
+#define PLAT_CPU_RELEASE_ADDR			0xffd12210
+
 /* Define next boot image name and offset */
 #define PLAT_NS_IMAGE_OFFSET			0x10000000
 #define PLAT_HANDOFF_OFFSET			0xFFE3F000
