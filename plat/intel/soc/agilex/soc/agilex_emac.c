7c13bcb652fba22d6675b3662a3ecbe88fe8fe73
Hadi Asyrafi <muhammad.hadi.asyrafi.abdul.halim@intel.com>
2019-12-23 17:58:04 +0800

    intel: System Manager refactoring
    Refactored system manager driver to be shared across both intel platform
    
    Signed-off-by: Hadi Asyrafi <muhammad.hadi.asyrafi.abdul.halim@intel.com>
    Change-Id: Ic4d056c3d15c3152403dc11641c2452770a6162d

--- plat/intel/soc/agilex/soc/agilex_emac.c
+++ plat/intel/soc/agilex/soc/agilex_emac.c
@@ -7,7 +7,7 @@
 #include <mmio.h>
 #include <platform_def.h>
 #include "socfpga_reset_manager.h"
-#include <agilex_system_manager.h>
+#include "socfpga_system_manager.h"
 
 void agx_emac_init()
 {
@@ -16,20 +16,17 @@ void agx_emac_init()
 		RSTMGR_PER0MODRST_EMAC1 |
 		RSTMGR_PER0MODRST_EMAC2);
 
-	mmio_clrsetbits_32(AGX_SYSMGR_CORE(SYSMGR_CORE_EMAC_0),
-		SYSMGR_CORE_EMAC_PHY_INTF_SEL_MSK,
-		EMAC0_PHY_MODE);
-	mmio_clrsetbits_32(AGX_SYSMGR_CORE(SYSMGR_CORE_EMAC_1),
-		SYSMGR_CORE_EMAC_PHY_INTF_SEL_MSK,
-		EMAC1_PHY_MODE);
-	mmio_clrsetbits_32(AGX_SYSMGR_CORE(SYSMGR_CORE_EMAC_2),
-		SYSMGR_CORE_EMAC_PHY_INTF_SEL_MSK,
-		EMAC2_PHY_MODE);
+	mmio_clrsetbits_32(SOCFPGA_SYSMGR(EMAC_0),
+		PHY_INTF_SEL_MSK, EMAC0_PHY_MODE);
+	mmio_clrsetbits_32(SOCFPGA_SYSMGR(EMAC_1),
+		PHY_INTF_SEL_MSK, EMAC1_PHY_MODE);
+	mmio_clrsetbits_32(SOCFPGA_SYSMGR(EMAC_2),
+		PHY_INTF_SEL_MSK, EMAC2_PHY_MODE);
 
-	mmio_clrbits_32(AGX_SYSMGR_CORE(SYSMGR_CORE_FPGAINTF_EN_3),
-		SYSMGR_CORE_FPGAINTF_EN_3_EMAC_MSK(0) |
-		SYSMGR_CORE_FPGAINTF_EN_3_EMAC_MSK(1) |
-		SYSMGR_CORE_FPGAINTF_EN_3_EMAC_MSK(2));
+	mmio_clrbits_32(SOCFPGA_SYSMGR(FPGAINTF_EN_3),
+		FPGAINTF_EN_3_EMAC_MSK(0) |
+		FPGAINTF_EN_3_EMAC_MSK(1) |
+		FPGAINTF_EN_3_EMAC_MSK(2));
 
 	mmio_clrbits_32(SOCFPGA_RSTMGR(PER0MODRST),
 		RSTMGR_PER0MODRST_EMAC0 |
