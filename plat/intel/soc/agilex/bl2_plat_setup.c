9cb928da525c24f539b28cc2e594bb3fa135059d
Hadi Asyrafi <muhammad.hadi.asyrafi.abdul.halim@intel.com>
2019-12-30 16:00:30 +0800

    intel: Change boot source selection
    Platform handoff structure no longer includes boot source selection.
    Hence, those settings can now be configured through socfpga_plat_def.h.
    
    Signed-off-by: Hadi Asyrafi <muhammad.hadi.asyrafi.abdul.halim@intel.com>
    Change-Id: If7ec6a03bb25156a6670ebf8f77105c370b553f6

--- plat/intel/soc/agilex/bl2_plat_setup.c
+++ plat/intel/soc/agilex/bl2_plat_setup.c
@@ -47,7 +47,7 @@ const mmap_region_t agilex_plat_mmap[] = {
 	{0},
 };
 
-boot_source_type boot_source;
+boot_source_type boot_source = BOOT_SOURCE;
 
 void bl2_el3_early_platform_setup(u_register_t x0, u_register_t x1,
 				u_register_t x2, u_register_t x4)
@@ -60,7 +60,6 @@ void bl2_el3_early_platform_setup(u_register_t x0, u_register_t x1,
 	if (socfpga_get_handoff(&reverse_handoff_ptr))
 		return;
 	config_pinmux(&reverse_handoff_ptr);
-	boot_source = reverse_handoff_ptr.boot_source;
 	config_clkmgr_handoff(&reverse_handoff_ptr);
 
 	enable_nonsecure_access();
