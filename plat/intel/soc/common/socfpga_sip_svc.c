c09a4b2416eefd92ef511842dc03900043862124
Chee Hong Ang <chee.hong.ang@intel.com>
2020-06-02 15:24:47 +0800

    intel: smc: Change SMC return arguments for INTEL_SIP_SMC_MBOX_SEND_CMD
    'INTEL_SIP_SMC_MBOX_SEND_CMD' SMC runtime service will only return
    mailbox status and the argument's length back to the caller.
    
    Signed-off-by: Chee Hong Ang <chee.hong.ang@intel.com>

--- plat/intel/soc/common/socfpga_sip_svc.c
+++ plat/intel/soc/common/socfpga_sip_svc.c
@@ -596,7 +596,7 @@ uintptr_t sip_smc_handler(uint32_t smc_fid,
 		status = intel_mbox_send_cmd(x1, (uint32_t *)x2, x3, x4,
 					     (uint32_t *)x5, x6, &mbox_status,
 					     &len_in_resp);
-		SMC_RET4(handle, status, mbox_status, x5, len_in_resp);
+		SMC_RET3(handle, status, mbox_status, len_in_resp);
 
 	case INTEL_SIP_SMC_FCS_CRYPTION:
 		x5 = SMC_GET_GP(handle, CTX_GPREG_X5);
