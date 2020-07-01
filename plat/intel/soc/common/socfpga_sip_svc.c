bef69138e26126aae305dfd084909b65a63fb72f
Chee Hong Ang <chee.hong.ang@intel.com>
2020-07-01 14:22:25 +0800

    intel: sip: Add RSU 'Max Retry' SiP SMC services
    Add SiP SMC services to store/retrieve 'Max Retry' counter
    for Remote System Update (RSU).
    
    Signed-off-by: Chee Hong Ang <chee.hong.ang@intel.com>

--- plat/intel/soc/common/socfpga_sip_svc.c
+++ plat/intel/soc/common/socfpga_sip_svc.c
@@ -28,6 +28,9 @@ static bool is_full_reconfig;
 /* RSU DCMF version */
 static uint32_t rsu_dcmf_ver[4] = {0};
 
+/* RSU Max Retry */
+static uint32_t rsu_max_retry = 0;
+
 /*  SiP Service UUID */
 DEFINE_SVC_UUID2(intl_svc_uid,
 		0xa85273b0, 0xe85a, 0x4862, 0xa6, 0x2a,
@@ -601,6 +604,13 @@ uintptr_t sip_smc_handler(uint32_t smc_fid,
 	case INTEL_SIP_SMC_RSU_COPY_DCMF_VERSION:
 		status = intel_rsu_copy_dcmf_version(x1, x2);
 		SMC_RET1(handle, status);
+
+	case INTEL_SIP_SMC_RSU_MAX_RETRY:
+		SMC_RET2(handle, INTEL_SIP_SMC_STATUS_OK, rsu_max_retry);
+
+	case INTEL_SIP_SMC_RSU_COPY_MAX_RETRY:
+		rsu_max_retry = x1;
+		SMC_RET1(handle, INTEL_SIP_SMC_STATUS_OK);
 
 	case INTEL_SIP_SMC_SERVICE_COMPLETED:
 		status = intel_smc_service_completed(x1, x2, &rcv_id,
