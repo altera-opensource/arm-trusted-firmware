9fe662ca3f5622621f0f585e8553e0c8a8affa43
Abdul Halim, Muhammad Hadi Asyrafi <muhammad.hadi.asyrafi.abdul.halim@intel.com>
2020-08-05 22:12:23 +0800

    intel: common: Refactor NOC header
    Refactor NOC header to be shareable across both Stratix 10 and Agilex
    platforms. This patch also removes redundant NOC declarations in system
    manager header file.
    
    Signed-off-by: Abdul Halim, Muhammad Hadi Asyrafi <muhammad.hadi.asyrafi.abdul.halim@intel.com>
    Change-Id: I6348b67a8b54c2ad19327d6b8c25ae37d25e4b4a

--- plat/intel/soc/agilex/include/socfpga_plat_def.h
+++ plat/intel/soc/agilex/include/socfpga_plat_def.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2019, ARM Limited and Contributors. All rights reserved.
- * Copyright (c) 2019, Intel Corporation. All rights reserved.
+ * Copyright (c) 2019-2020, Intel Corporation. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -15,6 +15,8 @@
 #define BOOT_SOURCE				BOOT_SOURCE_SDMMC
 
 /* Register Mapping */
+#define SOCFPGA_CCU_NOC_REG_BASE		0xf7000000
+
 #define SOCFPGA_MMC_REG_BASE			0xff808000
 
 #define SOCFPGA_RSTMGR_REG_BASE			0xffd11000
