f76831fed08aa35daff5c22935ac9072f705c2b7
Abdul Halim, Muhammad Hadi Asyrafi <muhammad.hadi.asyrafi.abdul.halim@intel.com>
2020-08-06 10:21:54 +0800

    intel: common: Create source file for firewall configuration
    Move codes that previously were part of system_manager driver into
    firewall driver which are more appropriate based on their functionalities.
    
    Signed-off-by: Abdul Halim, Muhammad Hadi Asyrafi <muhammad.hadi.asyrafi.abdul.halim@intel.com>
    Change-Id: I35e9d792f35ee7491c2f306781417a0c8faae3fd

--- plat/intel/soc/common/include/socfpga_system_manager.h
+++ plat/intel/soc/common/include/socfpga_system_manager.h
@@ -40,20 +40,9 @@
 #define IDLE_DATA_SOC2FPGA				BIT(4)
 #define IDLE_DATA_MASK		(IDLE_DATA_LWSOC2FPGA | IDLE_DATA_SOC2FPGA)
 
-#define SCR_AXI_AP_MASK					BIT(24)
-#define SCR_FPGA2SOC_MASK				BIT(16)
-#define SCR_MPU_MASK					BIT(0)
-#define DISABLE_L4_FIREWALL	(SCR_AXI_AP_MASK | SCR_FPGA2SOC_MASK \
-					| SCR_MPU_MASK)
-#define DISABLE_BRIDGE_FIREWALL				0x0ffe0101
-
 /* Macros */
 
 #define SOCFPGA_SYSMGR(_reg)		(SOCFPGA_SYSMGR_REG_BASE \
 						+ (SOCFPGA_SYSMGR_##_reg))
-
-
-void enable_ns_peripheral_access(void);
-void enable_ns_bridge_access(void);
 
 #endif /* SOCFPGA_SYSTEMMANAGER_H */
