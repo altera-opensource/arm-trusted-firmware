b852491c143c32ab8c74944822841b09b841efa5
Loh Tien Hock <tien.hock.loh@intel.com>
2021-04-14 17:21:43 +0800

    plat/intel: F2S bridge SMC enable and disable
    To properly enable or disable the bridges in SMC call or during reset,
    we need to run the F2S bridge enable or disable sequence.
    
    Signed-off-by: Loh Tien Hock <tien.hock.loh@intel.com>

--- plat/intel/soc/common/soc/socfpga_reset_manager.c
+++ plat/intel/soc/common/soc/socfpga_reset_manager.c
@@ -5,12 +5,14 @@
  */
 
 #include <common/debug.h>
+#include <drivers/delay_timer.h>
 #include <errno.h>
 #include <lib/mmio.h>
 
 #include "socfpga_mailbox.h"
 #include "socfpga_reset_manager.h"
 #include "socfpga_system_manager.h"
+#include "socfpga_f2sdram_manager.h"
 
 
 void deassert_peripheral_reset(void)
@@ -108,12 +110,44 @@ int socfpga_bridges_enable(void)
 	mmio_clrbits_32(SOCFPGA_RSTMGR(BRGMODRST), ~0);
 
 	/* Wait until idle ack becomes 0 */
-	return poll_idle_status(SOCFPGA_SYSMGR(NOC_IDLEACK),
-				IDLE_DATA_MASK, 0);
+	if (poll_idle_status(SOCFPGA_SYSMGR(NOC_IDLEACK),
+				IDLE_DATA_MASK, 0))
+	{
+		ERROR ("S2F bridge enable: Timedout waiting for idle ack");
+	}
+
+	/* F2S bridge enable */
+#if PLATFORM_MODEL == PLAT_SOCFPGA_STRATIX10
+	mmio_clrbits_32(SOCFPGA_RSTMGR(BRGMODRST),
+		RSTMGR_FIELD(BRG, FPGA2SOC) | RSTMGR_FIELD(BRG, F2SSDRAM0) |
+		RSTMGR_FIELD(BRG, F2SSDRAM1) | RSTMGR_FIELD(BRG, F2SSDRAM2));
+#else
+	mmio_clrbits_32(SOCFPGA_RSTMGR(BRGMODRST),
+		RSTMGR_FIELD(BRG, FPGA2SOC));
+#endif
+
+	mmio_clrbits_32(SOCFPGA_F2SDRAMMGR(SIDEBANDMGR_FLAGOUTSET0),
+		FLAGOUTCLR0_F2S_IDLEREQ);
+
+	if (poll_idle_status(SOCFPGA_F2SDRAMMGR(SIDEBANDMGR_FLAGINSTATUS0),
+		FLAGINTSTATUS0_F2S_IDLEACK, 0))
+	{
+		ERROR("F2S bridge enable: Timed out waiting for noc idle ack");
+	}
+
+	mmio_clrbits_32(SOCFPGA_F2SDRAMMGR(SIDEBANDMGR_FLAGOUTSET0),
+			FLAGOUTSET0_F2S_FORCE_DRAIN);
+
+	mmio_setbits_32(SOCFPGA_F2SDRAMMGR(SIDEBANDMGR_FLAGOUTSET0),
+			FLAGOUTSET0_F2S_ENABLE);
+
+	return 0;
 }
 
 int socfpga_bridges_disable(void)
 {
+	int timeout=100;
+
 	/* Set idle request */
 	mmio_write_32(SOCFPGA_SYSMGR(NOC_IDLEREQ_SET), ~0);
 
@@ -141,6 +175,47 @@ int socfpga_bridges_disable(void)
 
 	/* Disable NOC timeout */
 	mmio_clrbits_32(SOCFPGA_SYSMGR(NOC_TIMEOUT), 1);
+
+	/* Preparing to disable F2S bridge */
+	mmio_setbits_32(SOCFPGA_RSTMGR(HDSKEN), RSTMGR_HDSKEN_FPGAHSEN);
+
+	mmio_setbits_32(SOCFPGA_RSTMGR(HDSKREQ), RSTMGR_HDSKREQ_FPGAHSREQ);
+
+	poll_idle_status(SOCFPGA_RSTMGR(HDSKACK),
+			RSTMGR_HDSKACK_FPGAHSACK_MASK,
+			RSTMGR_HDSKACK_FPGAHSACK_MASK);
+
+
+	mmio_clrbits_32(SOCFPGA_F2SDRAMMGR(SIDEBANDMGR_FLAGOUTSET0),
+			FLAGOUTSET0_F2S_ENABLE);
+
+	mmio_setbits_32(SOCFPGA_F2SDRAMMGR(SIDEBANDMGR_FLAGOUTSET0),
+		FLAGOUTSET0_F2S_FORCE_DRAIN);
+
+	do {
+		uint32_t idle_status;
+		idle_status = mmio_read_32(SOCFPGA_F2SDRAMMGR(
+			SIDEBANDMGR_FLAGINSTATUS0) |
+			FLAGINSTATUS0_F2S_IDLE_STATUS);
+		if (idle_status == FLAGINSTATUS0_F2S_IDLE_STATUS)
+		{
+			idle_status = mmio_read_32(SOCFPGA_F2SDRAMMGR(
+				SIDEBANDMGR_FLAGINSTATUS0)) |
+				FLAGINSTATUS0_F2S_IDLE_STATUS;
+			if (idle_status == FLAGINSTATUS0_F2S_IDLE_STATUS)
+				break;
+		}
+		udelay (1000);
+	} while (timeout--> 0);
+
+	mmio_setbits_32(SOCFPGA_RSTMGR(BRGMODRST),
+		RSTMGR_FIELD(BRG, FPGA2SOC));
+
+	mmio_clrbits_32(SOCFPGA_RSTMGR(HDSKREQ),
+		RSTMGR_HDSKEQ_FPGAHSREQ);
+
+	mmio_setbits_32(SOCFPGA_F2SDRAMMGR(SIDEBANDMGR_FLAGOUTCLR0),
+		FLAGOUTCLR0_F2S_IDLEREQ);
 
 	return 0;
 }
