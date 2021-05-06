57c5d6ea7b2a198e4d8c80e194ccaed6c4ba6ee2
Boon Khai Ng <boon.khai.ng@intel.com>
2021-05-06 12:12:46 +0800

    intel: fcs: Initial commit for attestation service
    This patch extends the functionality of FPGA Crypto Service (FCS) to
    support FPGA Attestation feature in Stratix10 device.
    
    Change-Id: Ib15783383dc9a06a2f0dc6dc1786f44b89f32cb1
    Signed-off-by: Boon Khai Ng <boon.khai.ng@intel.com>
    
    Conflicts:
    
    	plat/intel/soc/common/include/socfpga_mailbox.h

--- plat/intel/soc/common/include/socfpga_mailbox.h
+++ plat/intel/soc/common/include/socfpga_mailbox.h
@@ -43,6 +43,7 @@
 #define MBOX_CMD_VAB_SRC_CERT		0x0B
 #define MBOX_CMD_GET_IDCODE		0x10
 #define MBOX_CMD_GET_USERCODE		0x13
+#define MBOX_CMD_GET_CHIPID		0x12
 #define MBOX_CMD_REBOOT_HPS		0x47
 
 /* Reconfiguration Commands */
@@ -69,6 +70,12 @@
 #define MBOX_FCS_DECRYPT_REQ		0x7F
 #define MBOX_FCS_RANDOM_GEN		0x80
 
+/* PSG SIGMA Commands */
+#define MBOX_PSG_SIGMA_TEARDOWN		0xD5
+
+/* Attestation Commands */
+#define MBOX_ATTESTATION_SUBKEY		0x182
+#define MBOX_GET_MEASUREMENT		0x183
 
 /* Mailbox Definitions */
 
